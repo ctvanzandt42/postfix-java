45be5b99aa49790136b8839990c61faf3ef480a4
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-09-20 14:02:14 -0500

    added gitignore

--- ReversePolishCalc.java
+++ ReversePolishCalc.java
@@ -1,25 +1,22 @@
 
 public class ReversePolishCalc {
 
-    // You'll need a variable here to keep track of the top of the stack
-
-    // The array of the input string split up
-    private String[] tokens =
-
-    // The stack
+    private int topStack = -1;
+    private String[] tokens;
     private String[] stack;
 
     public double calculate(String input) {
 
         // 1. Use the String split method to split the string into tokens at the commas
-        tokens =
+        tokens = input.split(",");
 
         // 2. Allocate a stack as big as the number of tokens
-        stack = ...
+        stack = new String[tokens.length]
 
         // 3. write the algorithm
         for(int i = 0; i < tokens.length; ++i) {
             // calls to push() and pop() and do the math here
+
         }
 
         // 4. return the result
